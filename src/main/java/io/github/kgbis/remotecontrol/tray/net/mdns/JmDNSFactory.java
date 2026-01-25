9afb4c0a7e43fcbf7de07f06cd753917d10567de
Kike García <kike.g.garcia@gmail.com>
2026-01-25 13:24:49 +0100

    Fixed Windows 7 crash as mDNS is not available

--- src/main/java/io/github/kgbis/remotecontrol/tray/net/mdns/JmDNSFactory.java
+++ src/main/java/io/github/kgbis/remotecontrol/tray/net/mdns/JmDNSFactory.java
@@ -21,11 +21,161 @@
 package io.github.kgbis.remotecontrol.tray.net.mdns;
 
 import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
+import javax.jmdns.ServiceTypeListener;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.Map;
 
+/**
+ * @noinspection RedundantThrows
+ */
 public interface JmDNSFactory {
 
-	JmDNS create(InetAddress addr) throws IOException;
+    JmDNS create(InetAddress inetAddress) throws IOException;
 
+    default JmDNS createDummy(InetAddress inetAddress) throws IOException {
+        return new JmDNS() {
+            @Override
+            public String getName() {
+                return "dummy-" + inetAddress.getHostAddress();
+            }
+
+            @Override
+            public String getHostName() {
+                return inetAddress.getHostName();
+            }
+
+            @Override
+            public InetAddress getInetAddress() throws IOException {
+                return inetAddress;
+            }
+
+            @Override
+            public InetAddress getInterface() throws IOException {
+                return inetAddress;
+            }
+
+            @Override
+            public ServiceInfo getServiceInfo(String s, String s1) {
+                return null;
+            }
+
+            @Override
+            public ServiceInfo getServiceInfo(String s, String s1, long l) {
+                return null;
+            }
+
+            @Override
+            public ServiceInfo getServiceInfo(String s, String s1, boolean b) {
+                return null;
+            }
+
+            @Override
+            public ServiceInfo getServiceInfo(String s, String s1, boolean b, long l) {
+                return null;
+            }
+
+            @Override
+            public void requestServiceInfo(String s, String s1) {
+                // Not implemented
+            }
+
+            @Override
+            public void requestServiceInfo(String s, String s1, boolean b) {
+                // Not implemented
+            }
+
+            @Override
+            public void requestServiceInfo(String s, String s1, long l) {
+                // Not implemented
+            }
+
+            @Override
+            public void requestServiceInfo(String s, String s1, boolean b, long l) {
+                // Not implemented
+            }
+
+            @Override
+            public void addServiceTypeListener(ServiceTypeListener serviceTypeListener) throws IOException {
+                // Not implemented
+            }
+
+            @Override
+            public void removeServiceTypeListener(ServiceTypeListener serviceTypeListener) {
+                // Not implemented
+            }
+
+            @Override
+            public void addServiceListener(String s, ServiceListener serviceListener) {
+                // Not implemented
+            }
+
+            @Override
+            public void removeServiceListener(String s, ServiceListener serviceListener) {
+                // Not implemented
+            }
+
+            @Override
+            public void registerService(ServiceInfo serviceInfo) throws IOException {
+                // Not implemented
+            }
+
+            @Override
+            public void unregisterService(ServiceInfo serviceInfo) {
+                // Not implemented
+            }
+
+            @Override
+            public void unregisterAllServices() {
+                // Not implemented
+            }
+
+            @Override
+            public boolean registerServiceType(String s) {
+                return false;
+            }
+
+            @Override
+            public void printServices() {
+                // Not implemented
+            }
+
+            @Override
+            public ServiceInfo[] list(String s) {
+                return new ServiceInfo[0];
+            }
+
+            @Override
+            public ServiceInfo[] list(String s, long l) {
+                return new ServiceInfo[0];
+            }
+
+            @Override
+            public Map<String, ServiceInfo[]> listBySubtype(String s) {
+                return null;
+            }
+
+            @Override
+            public Map<String, ServiceInfo[]> listBySubtype(String s, long l) {
+                return null;
+            }
+
+            @Override
+            public Delegate getDelegate() {
+                return null;
+            }
+
+            @Override
+            public Delegate setDelegate(Delegate delegate) {
+                return null;
+            }
+
+            @Override
+            public void close() throws IOException {
+// Not implemented
+            }
+        };
+    }
 }
